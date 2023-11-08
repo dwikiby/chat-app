d468bfb02a30175a644100813d2c654ff342f4fa
Dwiki Ramadhan <dwikiakbarramadhan@getslurp.com>
2023-11-08 16:55:30 +0700

    fix: modified env settings

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
